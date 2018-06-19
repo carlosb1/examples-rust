6918b90eadd991d8448503ebde0a6134a6d6fbea
Carlos B <carlos.baezruiz@gmail.com>
2018-06-19 23:51:51 +0200

    Added first skeleton for the ftp server

--- others/ftp-server/src/main.rs
+++ others/ftp-server/src/main.rs
@@ -9,7 +9,20 @@ mod cmd;
 mod ftp;
 mod error;
 
+use std::io;
+use futures::prelude::async;
 
+#[async]
+fn server() -> io::Result<()> {
+    Ok(())
+}
+
+
+use tokio_core::reactor::Core;
 fn main() {
+    let mut core = Core::new().expect("Cannot create tokio Core");
+    if let Err(error) = core.run(server()) {
+        println!("Error running the server: {}", error);
+    }
     println!("Hello, world!");
 }
