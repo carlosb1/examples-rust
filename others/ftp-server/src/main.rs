dbe3d89bc26a4ce993f0d5522a48682b75ba5254
Carlos B <carlos.baezruiz@gmail.com>
2018-06-20 19:25:53 +0200

    example for async block task

--- others/ftp-server/src/main.rs
+++ others/ftp-server/src/main.rs
@@ -1,4 +1,5 @@
-#![feature(proc_macro, conservative_impl_trait, generators)]
+//#![feature(proc_macro, conservative_impl_trait, generators)]
+#![feature(proc_macro_non_items, conservative_impl_trait, generators)]
 extern crate bytes;
 extern crate futures_await as futures;
 extern crate tokio_core;
@@ -10,19 +11,25 @@ mod ftp;
 mod error;
 
 use std::io;
-use futures::prelude::async;
+use futures::prelude::*;
 
+/*
 #[async]
 fn server() -> io::Result<()> {
     Ok(())
 }
+*/
 
 
 use tokio_core::reactor::Core;
 fn main() {
     let mut core = Core::new().expect("Cannot create tokio Core");
-    if let Err(error) = core.run(server()) {
+    let server = async_block! {
+        Ok(())
+    };
+    let result: Result<_, io::Error> = core.run(server);
+    if let Err(error) = result {
         println!("Error running the server: {}", error);
     }
-    println!("Hello, world!");
+
 }
