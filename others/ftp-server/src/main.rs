205d1531ef78728c9ec3a8db935b4ad183c46402
Carlos B <carlos.baezruiz@gmail.com>
2018-06-20 19:38:29 +0200

    Added first code for ftp server which manage clients

--- others/ftp-server/src/main.rs
+++ others/ftp-server/src/main.rs
@@ -1,5 +1,4 @@
-//#![feature(proc_macro, conservative_impl_trait, generators)]
-#![feature(proc_macro_non_items, conservative_impl_trait, generators)]
+#![feature(proc_macro, conservative_impl_trait, generators)]
 extern crate bytes;
 extern crate futures_await as futures;
 extern crate tokio_core;
@@ -12,24 +11,41 @@ mod error;
 
 use std::io;
 use futures::prelude::*;
-
-/*
-#[async]
-fn server() -> io::Result<()> {
-    Ok(())
-}
-*/
-
-
 use tokio_core::reactor::Core;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use tokio_core::reactor::Handle;
+use tokio_core::net::TcpListener;
+
+use tokio_core::net::TcpStream;
+
+#[async]
+fn handle_client(stream: TcpStream) -> result::Result<(), ()> {
+    await!(client(stream)).map_err(|error| println("Error handling client: {}", error))
+}
+
+#[async]
+fn server(handle: Handle) -> io::Result<()> {
+    let port = 1234;
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127,0,0,1)), port);
+    let listener = TcpListener::bind(&addr,&handle)?;
+    println!("Waiting clients on port {}...",port);
+    #[async]
+    for (stream, addr) in listener.incoming() {
+        let address = format!("[address : {}]",addr);
+        println!("New client {}", address);
+        handle.spawn(handle_client(stream));
+        println!("Waiting another client...");
+    }
+    return Ok(());
+}
+
+
+
 fn main() {
     let mut core = Core::new().expect("Cannot create tokio Core");
-    let server = async_block! {
-        Ok(())
-    };
-    let result: Result<_, io::Error> = core.run(server);
-    if let Err(error) = result {
+    let handle = core.handle();
+    if let Err(error) = core.run(server(handle)) {
         println!("Error running the server: {}", error);
     }
-
+    
 }
