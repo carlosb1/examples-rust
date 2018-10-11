19795a0d0b61ffeee06d20c983645843b3239ee7
Carlos B <carlos.baezruiz@gmail.com>
2018-10-11 08:54:38 +0200

    Designed first example for hero REST API

--- hello-rocket/src/main.rs
+++ hello-rocket/src/main.rs
@@ -2,14 +2,41 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
-#[get("/")]
-fn index () -> &'static str {
-    "Hello, world"
+use rocket_contrib::{Json, Value};
+
+mod hero;
+use hero::{Hero};
+
+#[post("/", data="<hero>")]
+fn create (hero: Json<Hero>) -> Json<Hero> {
+    hero
 }
 
+#[get("/")]
+fn read() -> Json<Value> {
+    Json(json!([
+        "hero 1",
+        "hero 2"
+    ]))
+}
+
+#[put("/<id>", data="<hero>")]
+fn update (id: i32, hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<Value>{
+    Json(json!({"status": "ok"}))
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read]).launch();
 }
 
 
