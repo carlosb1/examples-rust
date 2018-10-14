8fbd2b58e9292567a7cbcfc61c72e74432abc70c
Carlos B <carlos.baezruiz@gmail.com>
2018-10-14 23:10:13 +0200

    Added first example with diesel orm project

--- hello-rocket/src/main.rs
+++ hello-rocket/src/main.rs
@@ -1,9 +1,35 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+
+#[macro_use] extern crate diesel;
+extern crate dotenv;
+
+pub mod schema;
+pub mod models;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+// extern crate diesel_demo;
+//  use self::diesel_demo::*;
+use self::models::*;
+use self::diesel::prelude::*;
+
+
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
 extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
+
 
 use rocket_contrib::{Json, Value};
 
@@ -37,6 +63,18 @@ fn delete(id: i32) -> Json<Value>{
 fn main() {
     rocket::ignite().mount("/hero", routes![create, update, delete])
         .mount("/heroes", routes![read]).launch();
+    use diesel_demo::schema::posts::dsl::*;
+    // let connection = establish_connection();
+    // let results = posts.filter(published.eq(true)).limit(5).load::<Post>(&connection).expect("Error loading posts");
+
+    /*
+    println!("Displaying {} posts",results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("---------------\n")
+        println!("{}", post.body);
+    }
+    */
 }
 
 
