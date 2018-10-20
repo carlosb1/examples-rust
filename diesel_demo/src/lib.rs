65af7b47d4d7a810c2a256306a9d08b0bd621695
Carlos B <carlos.baezruiz@gmail.com>
2018-10-20 23:43:49 +0200

    Added example tow write post

--- diesel_demo/src/lib.rs
+++ diesel_demo/src/lib.rs
@@ -1,13 +1,16 @@
-pub mod schema;
-pub mod models;
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
+
+pub mod schema;
+pub mod models;
 
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
+
+use self::models::{Post, NewPost};
 
 pub fn establish_connection () -> PgConnection {
     dotenv().ok();
@@ -15,4 +18,15 @@ pub fn establish_connection () -> PgConnection {
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}",database_url))
 }
 
+pub fn create_post<'a> (conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+    use self::schema::posts;
 
+    let new_post = NewPost{
+        title: title,
+        body: body,
+    };
+
+    diesel::insert_into(posts::table).values(&new_post)
+            .get_result(conn)
+            .expect("Error saving new post")
+}
