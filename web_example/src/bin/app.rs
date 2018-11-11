99c08108ddfe9aa101e748f39dba8ffd74ad2061
Carlos B <carlos.baezruiz@gmail.com>
2018-11-11 23:39:39 +0100

    Added working rust rest service

--- web_example/src/bin/app.rs
+++ web_example/src/bin/app.rs
@@ -1,10 +1,17 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#![macro_use] extern crate rocket;
+extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
+
+#[macro_use]extern crate serde_derive;
+#[macro_use]extern crate serde_json;
+
+use rocket::{get, routes};
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    return("Hello, world!");
 } 
 
 fn main () {
