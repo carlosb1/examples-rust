e9acc15b8f6a491ecbd24781d90a48c472b1ed59
Carlos B <carlos.baezruiz@gmail.com>
2018-11-15 20:39:11 +0100

    Add first module to use db

---
+++ web_example/src/db/mod.rs
@@ -0,0 +1,41 @@
+use dotenv::dotenv;
+use std::ops::Deref;
+
+use std::env;
+use diesel::pg::PgConnection;
+use diesel::r2d2::{self, ConnectionManager};
+
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Outcome, Request, State};
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+pub fn init_pool() -> Pool {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    r2d2::Pool::new(manager).expect("db pool");
+}
+
+pub struct Conn(pub r2d2::PooledConnection<ConnectionManager<PgConnection>>);
+
+impl Deref for Conn {
+    type Target = PgConnection;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+
+}
+impl <'a, 'r> FromRequest<'a, 'r> for Conn {
+    type Error = ();
+    fn from_request(request: &'a Request<'r>) -> request.:Outcome<Conn, ()> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(Conn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+        }
+    }
+}
+
