9c37fd106cc48242503de891d9f92bf6724d05d5
Carlos B <carlos.baezruiz@gmail.com>
2018-11-21 22:37:33 +0100

    Added code to set up main

--- web_example/src/main.rs
+++ web_example/src/main.rs
@@ -1,5 +1,19 @@
+#![feature(plugin, custom_derive)]
+#[macro_use]extern crate rocket;
+#[macro_use]extern crate serde_json;
+#[macro_use]extern crate serde_derive;
+#[macro_use]extern crate rocket_contrib;
+
+mod routes;
+
+
+use std::sync::Mutex;
+use std::collections::HashMap;
+
+
 
 fn main ()  {
-    println!("hello world");
-
+    rocket::ignite().mount("/", routes![routes::index, routes::get, 
+                           routes::new, routes::update])
+        .register(catchers![routes::not_found]).manage(Mutex::new(HashMap::<routes::ID, String>::new())).launch();
 }
