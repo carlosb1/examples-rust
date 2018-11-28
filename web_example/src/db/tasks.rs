abce31ffd9569a6bc4dae12452eeae57821fa768
Carlos B <carlos.baezruiz@gmail.com>
2018-11-28 19:29:17 +0100

    Added code to db with tasks

--- web_example/src/db/tasks.rs
+++ web_example/src/db/tasks.rs
@@ -2,7 +2,7 @@ use diesel;
 use diesel::prelude::*;
 use schema::tasks;
 use diesel::pg::PgConnection;
-use models::task::{Task};
+use models::task::{Task, TaskJson};
 
 #[derive(Insertable)]
 #[table("tasks")]
@@ -26,5 +26,16 @@ pub fn create (
 
     diesel::insert_into(tasks::table).values(new_task).get_result::<Task>(conn).expect("Error creating task");
     // TODO return json
+}
+
+pub struct FindTasks {
+    title: Option<String>,
+    limit: Option<i64>, 
+    offset: Option<i64>,
+}
+
+pub fn find(conn:: &PgConnection, params: FindTasks) -> Vec<TaskJson> {
+    // let mut query = tasks::table.select().into_boxed
+    // let result = query.limit();
 }
 
--- web_example/src/models/task.rs
+++ web_example/src/models/task.rs
@@ -8,3 +8,18 @@ pub struct Task {
     pub done: bool,
 }
 
+impl Task {
+    pub fn attach(self) -> TaskJson {
+        return(TaskJson {id: self.id, title: self.title
+            , body: self.body, done: self.done});
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TaskJson {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub done: bool,
+}
