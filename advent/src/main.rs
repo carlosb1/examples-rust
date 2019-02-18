2b72c17dbadf56afee35aa542d04fcadedda1aa6
Carlos B <mcflurry0@gmail.com>
2019-02-18 16:32:04 +0100

    Added advent day 3

--- advent/src/main.rs
+++ advent/src/main.rs
@@ -2,12 +2,27 @@ extern crate regex;
 
 use std::io::{self, BufRead};
 use std::collections::HashMap;
+use std::fmt;
 
 pub struct Entry {
+    num: i32,
     index_x: i32,
     index_y: i32,
     width: i32,
     height: i32
+}
+
+impl fmt::Display for Entry {
+    fn fmt(&self,fmt:  &mut fmt::Formatter) -> fmt::Result {
+        let str_num = "num=".to_string()+&self.num.to_string();
+        let str_index_x = "index_x=".to_string() + &self.index_x.to_string(); 
+        let str_index_y = "index_y=".to_string() + &self.index_y.to_string();
+        let str_w = "w=".to_string() + &self.width.to_string();
+        let str_h = "h=".to_string() + &self.height.to_string();
+        let parsed = str_num + " " + str_index_x.as_str() + " " +  str_index_y.as_str() + " " +  str_w.as_str() + " " +  str_h.as_str();
+        fmt.write_str(parsed.as_str())?;
+        Ok(())
+    }
 }
 
 fn code1() {
@@ -29,7 +44,6 @@ fn code1() {
     }
     println!("{:?}", result); 
 }
-
 
 fn code2() {
     let mut threes = 0;
@@ -65,20 +79,43 @@ fn code2() {
 }
 
 
-static DEFAULT: &str = ".";
+static DEFAULT: i32 = 0;
+static MULTIPLE:  i32 = -1;
 
-fn paint_matrix(matrix: &mut [[&str; 10]; 10]) {
+fn paint_matrix(matrix: &mut [[i32; 10]; 10]) {
     for (i, row) in  matrix.iter_mut().enumerate() {
         for (y, col) in row.iter_mut().enumerate() {
-             print!("{}", col);  
+             if *col == 0 as i32 {
+                print!(".");
+             } else if *col == -1 as i32 {
+                print!("X");
+             } else {
+                print!("{}", col);  
+             }
         }
         println!();
     }
 }
 
+fn fill_matrix(matrix: &mut [[i32; 10]; 10], entry: &mut Entry) {
+    for x in entry.index_x..entry.width{
+        for y in entry.index_y..entry.height {
+            let current_value = matrix[x as usize][y as usize];
+            if current_value == DEFAULT {
+                 matrix[x as usize][y as usize] = entry.num; 
+                 //matrix[x as usize][y as usize] = "1"; 
+            } else {
+                if current_value != MULTIPLE {
+                    matrix[x as usize][y as usize] = MULTIPLE;
+                }
+            }
+        }
+    }
+}
+
 fn code3() {
     use regex::Regex;
-    let re = Regex::new(r"^#\d @ \d,\d: \dx\d").unwrap();
+    let re = Regex::new(r"^#(\d) @ (\d),(\d): (\d)x(\d)").unwrap();
     let mut vec: Vec<String> = Vec::new();
 
     let stdin = io::stdin();
@@ -91,13 +128,27 @@ fn code3() {
         vec.push(str_line);
     }
     // check input
+    let mut entries: Vec<Entry> = Vec::new();
     for line in vec {
         println!("{:?}", re.is_match(line.as_str()));
-        let cap = re.captures(line.as_str()).unwrap();
-        println!("{:?}",&cap[0]);
+        for cap in re.captures_iter(line.as_str()) {
+            let num: i32 = cap[1].parse::<i32>().unwrap();
+            let index_x: i32 = cap[2].parse::<i32>().unwrap();
+            let index_y: i32 = cap[3].parse::<i32>().unwrap();
+            let width: i32 = cap[4].parse::<i32>().unwrap();
+            let height: i32 = cap[5].parse::<i32>().unwrap();
+            let entry = Entry{num, index_x, index_y, width, height};
+            entries.push(entry);
+        }
     }
 
     let mut matrix = [[DEFAULT; 10];10];
+    
+    for mut ent in entries {
+        println!("{}",ent);
+        fill_matrix(&mut matrix, &mut ent);
+    }
+
     paint_matrix(&mut matrix);
 
 }
