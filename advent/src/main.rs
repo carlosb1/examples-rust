01c095845fc15de7d0338c394cda03e0cb33267d
Carlos B <mcflurry0@gmail.com>
2019-02-20 17:21:44 +0100

    Added first part for code day 4

--- advent/src/main.rs
+++ advent/src/main.rs
@@ -4,26 +4,7 @@ use std::io::{self, BufRead};
 use std::collections::HashMap;
 use std::fmt;
 
-pub struct Entry {
-    num: i32,
-    index_x: i32,
-    index_y: i32,
-    width: i32,
-    height: i32
-}
 
-impl fmt::Display for Entry {
-    fn fmt(&self,fmt:  &mut fmt::Formatter) -> fmt::Result {
-        let str_num = "num=".to_string()+&self.num.to_string();
-        let str_index_x = "index_x=".to_string() + &self.index_x.to_string(); 
-        let str_index_y = "index_y=".to_string() + &self.index_y.to_string();
-        let str_w = "w=".to_string() + &self.width.to_string();
-        let str_h = "h=".to_string() + &self.height.to_string();
-        let parsed = str_num + " " + str_index_x.as_str() + " " +  str_index_y.as_str() + " " +  str_w.as_str() + " " +  str_h.as_str();
-        fmt.write_str(parsed.as_str())?;
-        Ok(())
-    }
-}
 
 fn code1() {
     let stdin = io::stdin();
@@ -113,6 +94,28 @@ fn fill_matrix(matrix: &mut [[i32; 10]; 10], entry: &mut Entry) {
     }
 }
 
+pub struct Entry {
+    num: i32,
+    index_x: i32,
+    index_y: i32,
+    width: i32,
+    height: i32
+}
+
+impl fmt::Display for Entry {
+    fn fmt(&self,fmt:  &mut fmt::Formatter) -> fmt::Result {
+        let str_num = "num=".to_string()+&self.num.to_string();
+        let str_index_x = "index_x=".to_string() + &self.index_x.to_string(); 
+        let str_index_y = "index_y=".to_string() + &self.index_y.to_string();
+        let str_w = "w=".to_string() + &self.width.to_string();
+        let str_h = "h=".to_string() + &self.height.to_string();
+        let parsed = str_num + " " + str_index_x.as_str() + " " +  str_index_y.as_str() + " " +  str_w.as_str() + " " +  str_h.as_str();
+        fmt.write_str(parsed.as_str())?;
+        Ok(())
+    }
+}
+
+
 fn code3() {
     use regex::Regex;
     let re = Regex::new(r"^#(\d) @ (\d),(\d): (\d)x(\d)").unwrap();
@@ -153,9 +156,86 @@ fn code3() {
 
 }
 
+#[derive(PartialEq, Clone)]
+enum Event {
+    Shift,
+    Asleep,
+    Wakeup
+} 
+
+pub struct SoldierEvent {
+    event: Event,
+    num_worker: i32,
+    year: i32,
+    month: i32,
+    day: i32,
+    hour: i32,
+    minutes: i32
+
+}
+impl SoldierEvent  {
+    fn new( event: Event, num_worker: i32, year: i32, month: i32, day: i32, hour: i32, minutes: i32) -> SoldierEvent {
+        SoldierEvent {event: event, num_worker: num_worker, year: year, month: month, day: day, hour: hour, minutes: minutes}
+    }
+
+}
+
+
+fn code4 () {
+    use regex::Regex;
+    let mut re_shift = Regex::new(r"^\[(\d{4})\-(\d{2})\-(\d{2}) (\d{2}):(\d{2})\] Guard #(\d{2}) begins shift").unwrap();
+    let mut re_fall_sleeps = Regex::new(r"^\[(\d{4})\-(\d{2})\-(\d{2}) (\d{2}):(\d{2})\] falls asleep").unwrap();
+    let mut re_wakes_up = Regex::new(r"^\[(\d{4})\-(\d{2})\-(\d{2}) (\d{2}):(\d{2})\] wakes up").unwrap();
+
+    let mut vec: Vec<String> = Vec::new();
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let str_line = line.unwrap().trim().to_string();
+        if str_line == "0" {
+            println!("exit!");
+            break;
+        }
+        vec.push(str_line);
+    }
+    // check input
+    let mut entries: Vec<SoldierEvent> = Vec::new();
+    for line in vec {
+        let mut current_soldier = -1;
+        let typ: Event;
+        let re;
+        if (re_shift.is_match(line.as_str())) {
+           re = re_shift.clone();
+           typ = Event::Shift;
+        } else if (re_fall_sleeps.is_match(line.as_str())) {
+           re = re_fall_sleeps.clone();  
+           typ = Event::Asleep;
+        } else {
+           re = re_wakes_up.clone();  
+           typ = Event::Wakeup;
+        }
+        
+        for cap in re.captures_iter(line.as_str()) {    
+            let year: i32 = cap[1].parse::<i32>().unwrap();
+            let month: i32 = cap[2].parse::<i32>().unwrap();
+            let day: i32 = cap[3].parse::<i32>().unwrap();
+            let hour: i32 = cap[4].parse::<i32>().unwrap();
+            let minutes: i32 = cap[5].parse::<i32>().unwrap();
+            if(typ == Event::Shift) {
+                current_soldier = cap[6].parse::<i32>().unwrap();
+            }
+            entries.push(SoldierEvent::new(typ.clone(), current_soldier, year, month, day, hour, minutes));  
+        }
+    }
+    println!("events {:}", entries.len());
+
+    //TODO Finish code to search best sentynel
+}
+
 
 fn main() {
     //code1();
     //code2();
-    code3();
+    //code3();
+    code4();
 }
