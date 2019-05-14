555a483b89aac0e2bbca44d0cc2dd79947c0acb8
Carlos B <carlos.baezruiz@gmail.com>
2019-05-14 17:33:49 +0200

    Setting example for android

---
+++ android_example/set_up.sh
@@ -0,0 +1,7 @@
+export NDK_HOME='/home/carlosb/Desktop/tools/android-ndk-r19c'
+$NDK_HOME/build/tools/make_standalone_toolchain.py --api 26 --arch arm64 --install-dir NDK/arm64
+$NDK_HOME/build/tools/make_standalone_toolchain.py --api 26 --arch arm64 --install-dir NDK/arm
+$NDK_HOME/build/tools/make_standalone_toolchain.py --api 26 --arch arm64 --install-dir NDK/x86
+
+
+
