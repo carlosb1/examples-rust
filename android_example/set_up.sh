1e8c53990741e3d7ee3124e521c9ae53b5bf548f
Carlos B <carlos.baezruiz@gmail.com>
2019-05-15 18:47:15 +0200

    Added first example to compile in Android

--- android_example/set_up.sh
+++ android_example/set_up.sh
@@ -3,5 +3,12 @@ $NDK_HOME/build/tools/make_standalone_toolchain.py --api 26 --arch arm64 --insta
 $NDK_HOME/build/tools/make_standalone_toolchain.py --api 26 --arch arm64 --install-dir NDK/arm
 $NDK_HOME/build/tools/make_standalone_toolchain.py --api 26 --arch arm64 --install-dir NDK/x86
 
+# DEFINE CARGO RUST
+cp cargo-config.toml ~/.cargo/config
+rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android
+
+cargo new cargo
+mkdir android
+
 
 
