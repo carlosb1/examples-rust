631955cb2690050f46b8afb1335ba5ff676dc472
carlosb <mcflurry0@gmail.com>
2019-07-31 21:46:59 +0200

    Added first REST Service and end-to-end example

--- example_rest_v2/src/main.rs
+++ example_rest_v2/src/main.rs
@@ -1,9 +1,26 @@
+#![feature(proc_macro_hygiene)]
+#![feature(decl_macro)]
+#[macro_use] extern crate rocket;
+
+use rocket::local::Client;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello world"
+}
+
 fn main() {
     println!("Hello, world!");
+    rocket::ignite().mount("/", routes![hello]).launch();
 }
+
+
 
 #[test]
 fn test1() {
-    println!("this is my first test");
-    assert_eq!(1, 1);
+    let rocket = rocket::ignite().mount("/", routes![hello]);
+    let client = Client::new(rocket).unwrap();
+    let mut response = client.get("/").dispatch();
+    assert_eq!(response.body_string(), Some("Hello world".into()));
+
 }
