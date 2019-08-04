5380588241b96cf54d60319c56eb650ea555d4d3
carlosb <mcflurry0@gmail.com>
2019-08-05 01:33:36 +0200

    Added working example without polymorph

--- example_rest_v2/src/main.rs
+++ example_rest_v2/src/main.rs
@@ -2,11 +2,38 @@
 #![feature(decl_macro)]
 #[macro_use] extern crate rocket;
 
-use rocket::local::Client;
+use rocket::request::{Outcome, FromRequest};
+use rocket::Outcome::{Success};
+use rocket::Request;
+
+pub trait UseCase {
+    fn run(&self) -> &'static str;
+}
+
+
+
+struct HelloWorldCase {
+}
+
+
+impl UseCase for HelloWorldCase {
+    fn run(&self) -> &'static str {
+        "hello world case"
+    }
+}
+
+
+impl<'a, 'r> FromRequest<'a, 'r> for HelloWorldCase {
+    type Error = ();
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        Success(HelloWorldCase{})
+    }
+}
+
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello world"
+fn hello(use_case: HelloWorldCase) -> &'static str {
+    use_case.run()
 }
 
 fn main() {
@@ -18,7 +45,9 @@ fn main() {
 
 #[test]
 fn test1() {
-    let rocket = rocket::ignite().mount("/", routes![hello]);
+    use rocket::local::Client;
+    let use_case = HelloWorldCase{};
+    let rocket = rocket::ignite().manage(use_case).mount("/", routes![hello]);
     let client = Client::new(rocket).unwrap();
     let mut response = client.get("/").dispatch();
     assert_eq!(response.body_string(), Some("Hello world".into()));
