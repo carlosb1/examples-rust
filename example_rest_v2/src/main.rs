1a49e55f9eb89de7ffc642ae73e472759b974d23
carlosb <mcflurry0@gmail.com>
2019-08-11 20:05:26 +0200

    Added db classes

--- example_rest_v2/src/main.rs
+++ example_rest_v2/src/main.rs
@@ -1,11 +1,41 @@
 #![feature(proc_macro_hygiene)]
 #![feature(decl_macro)]
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate diesel;
+extern crate dotenv;
+
 
 use rocket::request::{Outcome, FromRequest};
 use rocket::Outcome::{Success};
 use rocket::Request;
 
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub mod schema; 
+
+
+// DATABASE classes
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+#[derive(Queryable)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub publihed: bool,
+}
+
+
+
+
+// Entities classes
 pub trait UseCase {
     fn run(&self) -> &'static str;
 }
