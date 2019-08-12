ac1be86d092641819b77661ea8a2426d3f7c5e2b
carlosb <mcflurry0@gmail.com>
2019-08-12 12:10:13 +0200

    Added db support

--- example_rest_v2/src/main.rs
+++ example_rest_v2/src/main.rs
@@ -14,16 +14,12 @@ use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
 
-pub mod schema; 
+pub mod schema;
+use schema::posts;
 
 
-// DATABASE classes
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
 
+#[derive(Debug)]
 #[derive(Queryable)]
 pub struct Post {
     pub id: i32,
@@ -32,7 +28,41 @@ pub struct Post {
     pub publihed: bool,
 }
 
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost <'a>{
+    pub title: &'a str,
+    pub body: &'a str,
 
+}
+
+// DB classes 
+pub struct DBPost {}
+
+impl DBPost {
+    // DATABASE classes
+        pub fn establish_connection(self) -> PgConnection {
+            dotenv().ok();
+            let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+            PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+        }
+
+        pub fn create(self, post: NewPost) -> Post {
+            let conn = self.establish_connection();
+            diesel::insert_into(posts::table).values(&post).get_result(&conn).expect("Error saving!")
+        }
+        pub fn read(self) -> Vec<Post> {
+            let conn = self.establish_connection();
+            posts::table.load::<Post>(&conn).unwrap()
+      }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for DBPost {
+    type Error = ();
+    fn from_request(_request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        Success(DBPost{})
+    }
+}
 
 
 // Entities classes
@@ -40,41 +70,62 @@ pub trait UseCase {
     fn run(&self) -> &'static str;
 }
 
-
+use std::rc::Rc;
 
 struct HelloWorldCase {
+    db: Rc<Option<DBPost>>
 }
 
+impl HelloWorldCase {
+    pub fn new() -> HelloWorldCase {
+        HelloWorldCase{db: Rc::new(None)}
+    }
+
+}
 
 impl UseCase for HelloWorldCase {
     fn run(&self) -> &'static str {
+        //unwrap shared reference
+        let values = Rc::try_unwrap(Rc::clone(&self.db));
+        let result = match values.unwrap_or(None)  {
+            Some(x) => x.read(),
+            None => Vec::new(),
+        };
+        println!("{:?}",result);
         "Hello world"
     }
 }
 
 impl<'a, 'r> FromRequest<'a, 'r> for HelloWorldCase {
     type Error = ();
-    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
-        Success(HelloWorldCase{})
+    fn from_request(_request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        Success(HelloWorldCase::new())
     }
 }
 
 #[get("/")]
-fn hello() -> &'static str {
-    HelloWorldCase{}.run()
+fn get(db: DBPost) -> &'static str {
+    HelloWorldCase{db: Rc::new(Some(db))}.run()
 }
+/*
+#[post("/", format="application/json", data="<post>")]
+fn post(db: DBPost, post: NewPost) -> &'static str {
+    "post"
+}
+*/
+
 
 fn main() {
-    println!("Hello, world!");
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![get]).launch();
 }
 
 
 #[test]
 fn test1() {
     use rocket::local::Client;
-    let use_case = HelloWorldCase{};
-    let rocket = rocket::ignite().mount("/", routes![hello]);
+//    let use_case = HelloWorldCase{};
+    let db = DBPost{};
+    let rocket = rocket::ignite().manage(db).mount("/", routes![get]);
     let client = Client::new(rocket).unwrap();
     let mut response = client.get("/").dispatch();
     assert_eq!(response.body_string(), Some("Hello world".into()));
