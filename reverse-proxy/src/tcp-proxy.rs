7d1528848947715f8ed099e4a7aacfe783f8b701
carlosb1 <mcflurry0@gmail.com>
2021-03-30 21:35:43 +0200

    added first getopts

--- reverse-proxy/src/tcp-proxy.rs
+++ reverse-proxy/src/tcp-proxy.rs
@@ -113,4 +113,33 @@ async fn forward(
     }
 }
 
-pub fn main() {}
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optopt("o", "", "set remote host for the forwarding", "REMOTE_HOST");
+    opts.optopt("i", "", "set local port  for the forwarding", "LOCAL_HOST");
+    opts.optopt(
+        "p",
+        "",
+        "set remote port  for the forwarding",
+        "REMOTE PORT",
+    );
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            panic!(f.to_string())
+        }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+}
