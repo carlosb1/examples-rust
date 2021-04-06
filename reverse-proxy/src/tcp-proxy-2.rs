a2239e3e11d99d93c2ce080764eb8b3034470575
carlosb1 <mcflurry0@gmail.com>
2021-04-06 23:48:43 +0200

    Added example for tcp proxy

---
+++ reverse-proxy/src/tcp-proxy-2.rs
@@ -0,0 +1,15 @@
+use tokio::net::TcpListener;
+
+use std::io;
+
+async fn process_socket<T>(socket: T) {}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        process_socket(socket).await;
+    }
+}
