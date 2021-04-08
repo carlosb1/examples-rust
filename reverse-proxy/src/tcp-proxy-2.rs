3de9dd18e01fe130b87c9662d10bef666104dd03
carlosb1 <mcflurry0@gmail.com>
2021-04-08 21:37:16 +0200

    Add function

--- reverse-proxy/src/tcp-proxy-2.rs
+++ reverse-proxy/src/tcp-proxy-2.rs
@@ -2,14 +2,17 @@ use tokio::net::TcpListener;
 
 use std::io;
 
-async fn process_socket<T>(socket: T) {}
+async fn process_socket<T>(socket: T, port: &str) {
+    TcpStream::connect();
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let output_port: &str = "";
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
     loop {
         let (socket, _) = listener.accept().await?;
-        process_socket(socket).await;
+        process_socket(socket, output_port).await;
     }
 }
