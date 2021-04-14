f8cdcabc55d0f4aa68e81e04ff9d34814d9844bb
carlosb1 <mcflurry0@gmail.com>
2021-04-14 21:06:12 +0200

    Created an proxy object

--- reverse-proxy/src/tcp-proxy.rs
+++ reverse-proxy/src/tcp-proxy.rs
@@ -15,35 +15,73 @@ use tokio::net::TcpSocket;
 use tokio::net::TcpStream;
 use tokio::task;
 
-use crate::config::load_config;
+use crate::config::{load_config, Host, RuleConfig};
 //TODO validate configuration
 //TODO validate host before to use
 
-async fn forward(mut stream_origin: TcpStream, output_address: &str) -> io::Result<()> {
-    let socket_output = TcpSocket::new_v4()?;
-    let mut stream_output = socket_output
-        .connect(output_address.parse().expect("Unable parse socket address"))
-        .await?;
-    let (mut stream_origin_recv, mut stream_origin_send) = stream_origin.split();
-    let (mut stream_output_recv, mut stream_output_send) = stream_output.split();
-    let (stream_origin_bytes_copied, stream_output_bytes_copied) = join!(
-        tokio::io::copy(&mut stream_origin_recv, &mut stream_output_send),
-        tokio::io::copy(&mut stream_output_recv, &mut stream_origin_send),
-    );
+pub struct Proxy {}
 
-    if let Ok(count) = stream_origin_bytes_copied {
-        info!("Stream origin transfered bytes: {}", count);
-    } else {
-        error!("Stream origin error transfering bytes");
+impl Proxy {
+    pub fn new() -> Proxy {
+        Proxy {}
     }
+    pub async fn forward(mut stream_origin: TcpStream, output_address: &str) -> io::Result<()> {
+        let socket_output = TcpSocket::new_v4()?;
+        let mut stream_output = socket_output
+            .connect(output_address.parse().expect("Unable parse socket address"))
+            .await?;
+        let (mut stream_origin_recv, mut stream_origin_send) = stream_origin.split();
+        let (mut stream_output_recv, mut stream_output_send) = stream_output.split();
+        let (stream_origin_bytes_copied, stream_output_bytes_copied) = join!(
+            tokio::io::copy(&mut stream_origin_recv, &mut stream_output_send),
+            tokio::io::copy(&mut stream_output_recv, &mut stream_origin_send),
+        );
 
-    if let Ok(count) = stream_output_bytes_copied {
-        info!("Stream output transfered bytes: {}", count);
-    } else {
-        error!("Stream output error transfering bytes");
+        if let Ok(count) = stream_origin_bytes_copied {
+            info!("Stream origin transfered bytes: {}", count);
+        } else {
+            error!("Stream origin error transfering bytes");
+        }
+
+        if let Ok(count) = stream_output_bytes_copied {
+            info!("Stream output transfered bytes: {}", count);
+        } else {
+            error!("Stream output error transfering bytes");
+        }
+
+        Ok(())
     }
+    pub async fn run(&self, rule_configs: &Vec<RuleConfig>) {
+        let mut joins = Vec::new();
+        for rule in rule_configs {
+            let input_host = rule.input.host.clone();
+            let input_port = rule.input.port;
+            let output_host = rule.output.host.clone();
+            let output_port = rule.output.port;
+            let rule_name = rule.name.clone();
 
-    Ok(())
+            let join = task::spawn(async move {
+                let output_address = format!("{}:{}", output_host, output_port);
+                let input_address = format!("{}:{}", input_host, input_port);
+                info!(
+                    "SERVICE {} - input address {} to output address {}",
+                    rule_name, input_address, output_address
+                );
+                let listener = TcpListener::bind(output_address.clone())
+                    .await
+                    .expect(format!("Error binding address {}", output_address.clone()).as_str());
+                //TODO loop to wait for connection, is it needed this loop?
+                loop {
+                    let (socket, _) = listener.accept().await.unwrap();
+                    Proxy::forward(socket, input_address.as_str())
+                        .await
+                        .unwrap();
+                }
+            });
+            joins.push(join);
+        }
+        join_all(joins).await;
+    }
 }
 
 fn print_usage(program: &str, opts: Options) {
@@ -81,34 +119,8 @@ async fn main() -> std::result::Result<(), std::io::Error> {
         return Ok(());
     }
     let config_file = matches.opt_str("f").unwrap();
-    let rule_configs = load_config(config_file.as_str());
-
-    let mut joins = Vec::new();
-    for rule in rule_configs {
-        let input_host = rule.input.host;
-        let input_port = rule.input.port;
-        let output_host = rule.output.host;
-        let output_port = rule.output.port;
-        let rule_name = rule.name;
-
-        let join = task::spawn(async move {
-            let output_address = format!("{}:{}", output_host, output_port);
-            let input_address = format!("{}:{}", input_host, input_port);
-            info!(
-                "SERVICE {} - input address {} to output address {}",
-                rule_name, input_address, output_address
-            );
-            let listener = TcpListener::bind(output_address.clone())
-                .await
-                .expect(format!("Error binding address {}", output_address.clone()).as_str());
-            //TODO loop to wait for connection, is it needed this loop?
-            loop {
-                let (socket, _) = listener.accept().await.unwrap();
-                forward(socket, input_address.as_str()).await.unwrap();
-            }
-        });
-        joins.push(join);
-    }
-    join_all(joins).await;
+    let rule_config = load_config(config_file.as_str());
+    let proxy = Proxy::new();
+    proxy.run(&rule_config).await;
     Ok(())
 }
