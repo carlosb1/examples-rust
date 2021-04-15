cae806084075feff721af2442d5465596d574709
carlosb1 <mcflurry0@gmail.com>
2021-04-15 21:33:55 +0200

    Splitted server

--- reverse-proxy/src/main.rs
+++ reverse-proxy/src/main.rs
@@ -2,7 +2,49 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 pub mod config;
+pub mod tcpproxy;
 
-fn main() {
-    println!("hello world");
+use crate::config::load_config;
+use crate::tcpproxy::Proxy;
+use getopts::Options;
+use std::env;
+
+#[tokio::main]
+async fn main() -> std::result::Result<(), std::io::Error> {
+    pretty_env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optopt(
+        "f",
+        "config.yml",
+        "specify configuration file for tcp rules",
+        "CONFIG_FILE_PROXY",
+    );
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            panic!(f.to_string())
+        }
+    };
+    let program = args[0].clone();
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return Ok(());
+    }
+
+    if !matches.opt_present("f") {
+        print_usage(&program, opts);
+        return Ok(());
+    }
+    let config_file = matches.opt_str("f").unwrap();
+    let rule_config = load_config(config_file.as_str());
+    let proxy = Proxy::new();
+    proxy.run(&rule_config).await;
+    Ok(())
+}
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    println!("{}", opts.usage(&brief));
 }
--- reverse-proxy/src/tcpproxy.rs
+++ reverse-proxy/src/tcpproxy.rs
@@ -1,21 +1,13 @@
-extern crate pretty_env_logger;
-#[macro_use]
-extern crate log;
-
-pub mod config;
-
-use std::env;
-use std::io;
-
 use futures::future::join_all;
-use getopts::Options;
+use std::io;
 use tokio::join;
 use tokio::net::TcpListener;
 use tokio::net::TcpSocket;
 use tokio::net::TcpStream;
 use tokio::task;
 
-use crate::config::{load_config, Host, RuleConfig};
+use crate::config::RuleConfig;
+
 //TODO validate configuration
 //TODO validate host before to use
 
@@ -48,7 +40,6 @@ impl Proxy {
         } else {
             error!("Stream output error transfering bytes");
         }
-
         Ok(())
     }
     pub async fn run(&self, rule_configs: &Vec<RuleConfig>) {
@@ -82,45 +73,4 @@ impl Proxy {
         }
         join_all(joins).await;
     }
-}
-
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} [options]", program);
-    println!("{}", opts.usage(&brief));
-}
-
-#[tokio::main]
-async fn main() -> std::result::Result<(), std::io::Error> {
-    pretty_env_logger::init();
-    let args: Vec<String> = env::args().collect();
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "print this help menu");
-    opts.optopt(
-        "f",
-        "config.yml",
-        "specify configuration file for tcp rules",
-        "CONFIG_FILE_PROXY",
-    );
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => {
-            panic!(f.to_string())
-        }
-    };
-    let program = args[0].clone();
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-        return Ok(());
-    }
-
-    if !matches.opt_present("f") {
-        print_usage(&program, opts);
-        return Ok(());
-    }
-    let config_file = matches.opt_str("f").unwrap();
-    let rule_config = load_config(config_file.as_str());
-    let proxy = Proxy::new();
-    proxy.run(&rule_config).await;
-    Ok(())
 }
