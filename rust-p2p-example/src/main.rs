e02d4c2170113c907928ace34bde6a40fc8d3f2a
carlosb1 <mcflurry0@gmail.com>
2022-09-27 23:26:23 +0200

    Add recipe support function
    Signed-off-by: carlosb1 <mcflurry0@gmail.com>

--- rust-p2p-example/src/main.rs
+++ rust-p2p-example/src/main.rs
@@ -115,10 +115,80 @@ fn respond_with_public_recipes(sender: mpsc::UnboundedSender<ListResponse>, rece
     });
 }
 
+async fn handle_create_recipe(cmd: &str) {
+    if let Some(rest) = cmd.strip_prefix("create r") {
+        let elements: Vec<&str> = rest.split("|").collect();
+        if elements.len() < 3 {
+            info!("too few arguments - Format: name|ingredients|instructions");
+        } else {
+            let name = elements.get(0).expect("name is there");
+            let ingredients = elements.get(1).expect("ingredients is there");
+            let instructions = elements.get(2).expect("instructions is there");
+            if let Err(e) = create_new_recipe(name, ingredients, instructions).await {
+                error!("error creating recipe: {}", e);
+            };
+        }
+    }
+}
+
+async fn handle_publish_recipe(cmd: &str) {
+    if let Some(rest) = cmd.strip_prefix("publish r") {
+        match rest.trim().parse::<usize>() {
+            Ok(id) => {
+                if let Err(e) = publish_recipe(id).await {
+                    info!("error publishing recipe with id {}, {}", id, e)
+                } else {
+                    info!("Published Recipe with id: {}", id);
+                }
+            }
+            Err(e) => error!("invalid id: {}, {}", rest.trim(), e),
+        };
+    }
+}
+
+async fn create_new_recipe(name: &str, ingredients: &str, instructions: &str) -> Result<()> {
+    let mut local_recipes = read_local_recipes().await?;
+    let new_id = match local_recipes.iter().max_by_key(|r| r.id) {
+        Some(v) => v.id + 1,
+        None => 0,
+    };
+    local_recipes.push(Recipe {
+        id: new_id,
+        name: name.to_owned(),
+        ingredients: ingredients.to_owned(),
+        instructions: instructions.to_owned(),
+        public: false,
+    });
+    write_local_recipes(&local_recipes).await?;
+
+    info!("Created recipe:");
+    info!("Name: {}", name);
+    info!("Ingredients: {}", ingredients);
+    info!("Instructions:: {}", instructions);
+
+    Ok(())
+}
+
+async fn publish_recipe(id: usize) -> Result<()> {
+    let mut local_recipes = read_local_recipes().await?;
+    local_recipes
+        .iter_mut()
+        .filter(|r| r.id == id)
+        .for_each(|r| r.public = true);
+    write_local_recipes(&local_recipes).await?;
+    Ok(())
+}
+
 async fn read_local_recipes() -> Result<Recipe> {
     let content = fs::read(STORAGE_FILE_PATH).await?;
     let result = serde_json::from_slice(&content)?;
     Ok(result)
+}
+
+async fn write_local_recipes(recipes: &Recipe) -> Result<()> {
+    let json = serde_json::to_string(&recipes)?;
+    fs::write(STORAGE_FILE_PATH, &json).await?;
+    Ok(())
 }
 
 async fn handle_list_peers(swarm: &mut Swarm<RecipeBehaviour>) {
